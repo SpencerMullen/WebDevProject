dcee62a761d7c08311bfaf0bf3c58aa3900822d2
SpencerMullen <spencermullen0@gmail.com>
2023-11-17 13:53:35 -0500

    Added tepmlate pages and set up browser router

--- web-dev-project/src/App.tsx
+++ web-dev-project/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Details from './components/pages/details/details'
+import Home from './components/pages/home/home'
+import Login from './components/pages/login/login'
+import Profile from './components/pages/profile/profile'
+import ProfileId from './components/pages/profileid/profileid'
+import Search from './components/pages/search/search'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/:id" element={<ProfileId />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
---
+++ web-dev-project/src/components/pages/profileid/profileid.tsx
@@ -0,0 +1,7 @@
+export default function ProfileId() {
+    return (
+        <>
+        <h1>ProfileId</h1>
+        </>
+    )
+}

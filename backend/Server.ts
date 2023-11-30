af00901deb4240a8597e5cbb2e400cb7699d7914
ethankong150 <kong.e@northeastern.edu>
2023-11-30 16:13:33 -0500

    backend initial

---
+++ backend/Server.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+const port = 8081
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+

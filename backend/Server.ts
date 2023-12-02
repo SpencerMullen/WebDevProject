a270b918d11f14fa4fb273b8f3c30fe6c00c009e
ethankong150 <kong.e@northeastern.edu>
2023-12-02 18:53:40 -0500

    removed old tmdb route  redundencies

--- backend/Server.ts
+++ backend/Server.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import tmdb_route from './routes/tmdb_route';
 import cors from "cors";
 import UsersRoutes from './routes/users';
 import MoviesRoutes from './routes/movies';
@@ -10,7 +9,6 @@ dotenv.config();
 const PORT = process.env.PORT || 8081;
 
 app.use(cors());
-//app.use('/tmdb', tmdb_route)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Nothing to see here!');
